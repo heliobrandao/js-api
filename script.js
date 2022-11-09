f5ce497e3f409add69b397be863439cec819dcdc
heliobrandao <heliob79@gmail.com>
2022-11-09 11:34:43 -0300

    lesson-03

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/13100218/json/');
+var consultaCEP = fetch('https://viacep.com.br/ws/13100218/json/')
+  .then(resposta => resposta.json())
+  .then(r => {
+    if (r.erro){
+      throw Error('Esse CEP não existe')
+    }else{
+      console.log(r)
+    }
+  })
+  .catch(erro => console.log(erro))
+  .finally(mensagem => console.log('Processamento concluído!'));
+
 console.log(consultaCEP);
