dccc78d6d28a28b848fce6c8573c56e99255857f
heliobrandao <heliob79@gmail.com>
2022-11-08 21:59:12 -0300

    lesson-01

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('Mandando oi para lega');
+
+function mandaMensagem() {
+  console.log('Tudo bem?');
+  console.log('Vou te mandar uma solicitação');
+  console.log('Solcitação recebida');
+}
+
+setTimeout(mandaMensagem, 5000);
+
+console.log('tchau!');
