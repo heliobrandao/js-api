958ddf9a96c9db9f942309e82d057673c0c2bfe0
heliobrandao <heliob79@gmail.com>
2022-11-09 10:53:44 -0300

    lesson-04.1

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/13100218/json/');
-console.log(consultaCEP);
+async function buscaEndereco() {
+  try{
+    var consultaCEP = await fetch('https://viacep.com.br/ws/13100218/json/');
+    var consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+      throw Error ('CEP não existente!')
+    }
+    console.log(consultaCEPConvertida);
+  } catch (erro) {
+    console.log(erro);
+  }
+
+}
+
+buscaEndereco();
